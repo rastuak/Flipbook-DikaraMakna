dbe0c6afe4c90a35b16a085ca6f565b455fa8743
rastuak <rastuakfaisal@gmail.com>
2024-11-04 16:49:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 import Flipbook from './components/Flipbook';
-
+import logo from './serigalapikra.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Flipbook/>
-      </header>
+    <div className="flex min-h-screen w-full justify-center items-center overflow-hidden">
+      <div className="h-screen w-full flex justify-center items-center">
+        <img src={logo} className="w-72" alt="logo" />
+      </div>
+      <Flipbook />
     </div>
   );
 }
--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react';
 import HTMLFlipBook from 'react-pageflip';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import '../Modal.css';
+import tagline from '../tagline.png';
 
 function Flipbook(props) {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
 
-    function onDocumentLoadSuccess({ numPages }) {
-      setNumPages(numPages);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  function pagesList() {
+    var pages = [];
+    for (var i = 1; i <= numPages; i++) {
+      pages.push(<div key={i}><Page width={500} pageNumber={i} /></div>);
     }
-    function pagesList(){
-      var pages = [];
-      for(var i=1; i<=numPages; i++){
-        pages.push(<div><Page width={500} pageNumber={i}/></div>);
-      }
-      return pages;
-    }
+    return pages;
+  }
 
-    return (
-        <Document
-        file="./Sample.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-        className='modal-90w'
-        >        
+  return (
+    <Document
+      file="./fikra.pdf"
+      onLoadSuccess={onDocumentLoadSuccess}
+      className='fixed top-0 left-0 w-full h-full bg-white z-50 flex flex-col justify-center items-center overflow-visible lg:overflow-hidden min-w-0 min-h-0 scale-75 lg:scale-100'
+    >
+      <img src={tagline} alt="tagline" className="w-80 my-0 lg:my-3 hidden lg:block" />
+      <div>
         <HTMLFlipBook width={500} height={707}>
-            {pagesList()}
+          {pagesList()}
         </HTMLFlipBook>
-        </Document>
+      </div>
+    </Document>
 
-    );
+  );
 }
 export default Flipbook;
